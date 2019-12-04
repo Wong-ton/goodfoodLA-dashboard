03e25f0aa3dbde6fb71ae20df912dad37762962f
Jason Wong <jwong175@hotmail.com>
2019-12-04 15:57:14 -0800

    Updates

---
+++ precache-manifest.298ba9d958d4c075af044ae1877e8b55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742f5e2a74ccd6e8fa48068c5e09678e",
+    "url": "/goodfoodLA-dashboard/index.html"
+  },
+  {
+    "revision": "5bac95351c7450832af4",
+    "url": "/goodfoodLA-dashboard/static/css/main.2188e002.chunk.css"
+  },
+  {
+    "revision": "65510c3dd2f4369058ac",
+    "url": "/goodfoodLA-dashboard/static/js/2.992ef907.chunk.js"
+  },
+  {
+    "revision": "5bac95351c7450832af4",
+    "url": "/goodfoodLA-dashboard/static/js/main.87e326f9.chunk.js"
+  },
+  {
+    "revision": "9be80ff816d15d300c3c",
+    "url": "/goodfoodLA-dashboard/static/js/runtime~main.e12ce9c7.js"
+  },
+  {
+    "revision": "10636bf7390dcf240bb15cb55799d10f",
+    "url": "/goodfoodLA-dashboard/static/media/HealthyHeaderbkgrd.10636bf7.png"
+  },
+  {
+    "revision": "49a9c5ff87b3d12460c972d6efecf295",
+    "url": "/goodfoodLA-dashboard/static/media/LOGO.49a9c5ff.png"
+  }
+]);
